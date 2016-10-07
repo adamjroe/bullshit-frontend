b19dacd6be03003b3af963ae08e0f9a0f86c26a8
adam <adam@kiron.university>
2016-10-07 23:14:42 +0200

    deleting things I dont understand

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,9 +3,6 @@ import { connect } from 'react-redux';
 
 import { Panel, Input } from 'react-bootstrap';
 
-import Todo from './Todo';
-import { createTodo, updateInput } from '../actions/ActionCreators';
-
 const styles = {
   width: '80%',
   maxWidth: 500,
@@ -14,24 +11,9 @@ const styles = {
 
 class App extends Component {
 
-  static propTypes = {
-    todos: PropTypes.arrayOf(Todo.propTypes).isRequired,
-    input: PropTypes.string.isRequired,
-    updateInput: PropTypes.func.isRequired,
-    createTodo: PropTypes.func.isRequired,
-  };
-
   constructor(props) {
     super(props);
-
-    this.handleNewTodo = this.handleNewTodo.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleNewTodo(event) {
-    event.preventDefault();
-    this.props.createTodo(this.props.input);
-    this.props.updateInput('');
   }
 
   handleInputChange() {
@@ -39,21 +21,9 @@ class App extends Component {
   }
 
   render() {
-    const todos = this.props.todos
-      .map((todo, index) => <Todo key={ `todo${index}` } { ...todo } />);
-
     return (
-      <Panel header={ <h3>Kiron Todo App</h3> } style={ styles } >
-        <form onSubmit={ this.handleNewTodo } >
-          <Input
-            ref="textField"
-            type="text"
-            placeholder="Enter new todo"
-            value={ this.props.input }
-            onChange={ this.handleInputChange }
-          />
-        </form>
-        { todos }
+      <Panel header={ <h1>Bullshit Bingo</h1>} style={ styles } >
+        <h3> Fun for the whole Kiron Family</h3>
       </Panel>
     );
   }
@@ -61,5 +31,4 @@ class App extends Component {
 
 export default connect(
   state => state,
-  { createTodo, updateInput }
 )(App);
